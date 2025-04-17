2ba889a7b6ad93e1485e8bf6032fb939c72cfaf2
ScottWMuir <157434980+ScottWMuir@users.noreply.github.com>
2025-04-17 16:11:47 -0600

    adding files

---
+++ Projects/Project7/myscripts.js
@@ -0,0 +1,56 @@
+	PrintButton1 = new Image
+	PrintButton2 = new Image
+	logo1 = new Image
+	logo2 = new Image
+if (document.images) {
+	PrintButton1.src = "Images/print.jpg"
+	PrintButton2.src = "Images/print2.jpg"
+	logo1.src = "Images/logo.jpg"
+	logo2.src = "Images/logo2.jpg"
+}
+
+
+		var imgArray = new Array(
+			'image1lg.jpg',
+			'image2lg.jpg',
+			'image3lg.jpg',
+			'image4lg.jpg'
+			
+
+		);
+
+
+		var titleArray = new Array(
+			'girl eating salad',
+			'girl eating jello',
+			'hamburger on grill',
+			'wine glass'
+			
+		);
+			
+		var imgPath = "images/fullsize/";
+		
+		function swapImage(imgID) {
+
+			var theImage = document.getElementById('theImage');
+			var textDiv = document.getElementById('bottomText');
+
+			var newImg;
+			var textTitle;
+
+			newImg = imgArray[imgID];
+			theImage.src = imgPath + newImg;
+
+			textTitle=titleArray[imgID];
+
+      			textDiv.innerHTML = textTitle;
+		}
+			
+		function preloadImages() {		
+			for(var i = 0; i < imgArray.length; i++) {
+				var tmpImg = new Image;
+				tmpImg.src = imgPath + imgArray[i];
+			}
+		}
+		
+
